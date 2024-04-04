9d34f8a946a5ec18361c3e00255e2c8671e81068
donga <dongan0618@naver.com>
2024-04-04 17:05:19 +0900

    app-configuration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ip } from './decorators/ip.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  private readonly logger=new Logger();
+
+  @Get('ip')
+  getHello(
+    @Ip() ip:string
+  ): string {
+    this. logger.log(ip);
+    this. logger.debug(ip);
+    this. logger.error(ip);
     return this.appService.getHello();
+    
   }
 }
